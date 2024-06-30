155caa7a3291da03e4ff1ea328714613281c2063
anthomars <anthomars.am@gmail.com>
2024-07-01 02:19:37 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Jenazah;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('pages.dashboard');
+        $data['kematian'] = Jenazah::whereMonth('tgl_wafat', Carbon::now()->month)->count();
+
+        return view('pages.dashboard', compact('data'));
     }
 }
